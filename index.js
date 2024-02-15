c76747749d30cd3120a79eef15c113ec7b4b0590
MrBarker47 <jebarker7@gmail.com>
2024-02-15 16:24:18 -0500

    added new files

---
+++ index.js
@@ -0,0 +1,17 @@
+//Create a function called getComputerChoice()
+//Randomly return rock paper scissors 
+function getComputerChoice() {
+    return 
+}
+
+//Create a playRound() function, with two parameters 
+//That returns a you lose! paper beats rock 
+
+
+//Create a new functions called playGame function
+
+
+/* Use the previous function inside of this one 
+play a five round games that keeps score and lets you know 
+a winner and loser at the end 
+*/
