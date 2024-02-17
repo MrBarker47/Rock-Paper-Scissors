c93b271a4fa003b5f84b94406c9c0eabd9ad26e5
MrBarker47 <jebarker7@gmail.com>
2024-02-17 14:34:39 -0500

    Updated the files

--- index.js
+++ index.js
@@ -1,14 +1,35 @@
 //Create a function called getComputerChoice()
 //Randomly return rock paper scissors 
 function getComputerChoice() {
-    return 
+    return  ['rock', 'paper', 'scissors']
+}
+
+let playerSelection = prompt("rock, paper, scissors shoot!"); 
+
+if(playerSelection === 'paper') {
+    console.log("You win! Paper beats rock")
+} else if (playerSelection === 'scissors') {
+    console.log("You Win! scissors beats paper")
+} else if (playerSelection === 'rock') {
+    console.log("You lose!  ")
 }
 
 //Create a playRound() function, with two parameters 
 //That returns a you lose! paper beats rock 
+function playRound(playerSelection, computerSelection) {
+
+}
+
+
+
 
 
 //Create a new functions called playGame function
+
+
+
+
+
 
 
 /* Use the previous function inside of this one 
