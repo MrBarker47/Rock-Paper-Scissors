2e4927adaa7b543f8c325e29438bb98ee2a0e44b
MrBarker47 <jebarker7@gmail.com>
2024-03-01 14:55:49 -0500

    Update files

--- index.js
+++ index.js
@@ -10,31 +10,57 @@ function getComputerChoice() {
         return random;
     }
 
-    //  let newGame = ['rock', 'paper', 'scissors'];
-    //  let result = getComputerChoice(newGame);
-    //  console.log(result);
+    
 
-     
-    function playRound(playerSelection, computerSelection) {
-
-      if(playerSelection == computerSelection) {
-        console.log("It's a tie");
-        } else if ((playerSelection === 'rock') && (computerSelection === 'scissors')) {
-            console.log('You Win! rock beats scissors');
-        } else if ((playerSelection === 'paper') && (computerSelection === 'scissors')) {
-            console.log('You Lose! Scissors beat paper')
-        } else if ((playerSelection === 'scissors') && (computerSelection === 'rock')) {
-            console.log('You Lose! Rock beats Scissors')
-        } else if ((playerSelection === 'rock') && (computerSelection === 'paper')) {
-            console.log('You Lose! Paper beats rock')
-        } else if ((playerSelection === ''))  {
-
-        }
-
-    }
   
 
-  let playerSelection = prompt("Rock, Paper, Scissors Shoot");
-  let computerSelection = getComputerChoice();
-  console.log(playRound(playerSelection, computerSelection));
+    function playRound(playerSelection, computerSelection) {
 
+        if(playerSelection === computerSelection) {
+            console.log('its a tie')     
+            } else if(playerSelection === 'rock' && computerSelection === 'paper') {
+            console.log('You Lose! Paper beats rock');
+            } else if (playerSelection === 'paper' && computerSelection === 'rock') {
+                console.log('You Win! Paper beats rock');
+            } else if (playerSelection === 'scissors' && computerSelection === 'paper') {
+                    console.log('You Win! Scissors beat paper');
+            } else if (playerSelection === 'rock' && computerSelection === 'scissors') {
+            console.log('You Win! Scissors beat rock');
+            } else if (playerSelection === 'paper' && computerSelection === 'scissors') {
+                console.log('You Lose! Scissors beat paper ') 
+            } else if (playerSelection === 'scissors' && computerSelection === 'rock') {
+            console.log('You Lose! Rock beats scissors')
+           
+        }
+     }
+
+           function playGame() {
+            for (let i = 0; i < 5; i++) {
+                console.log(playRound());
+            }
+           }
+
+           
+ 
+
+        let playerSelection = prompt("Rock, Paper, Scissors Shoot");
+        let computerSelection = getComputerChoice();
+        console.log(playRound(playerSelection, computerSelection));
+          
+
+         playGame();
+   
+
+
+  
+  
+
+
+
+
+
+
+
+
+
+ 
